07bb3f2a43a25f4ce45bd78ee0a66facd3e714ed
jaiswalk008 <jaiskaran008@gmail.com>
2023-11-07 20:37:46 +0530

    added restaurant summary

--- src/Components/Layout/Header.js
+++ src/Components/Layout/Header.js
@@ -7,7 +7,16 @@ const Header = () =>{
                 <h1><img src='https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRzJUbVeo54US84XD82FvvD20BU_3ri7-TjGXv87bE&s' alt="logo"></img>Pizza Mania</h1>
                 <Cart/>
             </nav>
-            <img className='header-image' src={require('./Header-image.jpg')} alt='pizza mania Header' />
+            <div style={{ position: 'relative' }}>
+                <img className='header-image' src={require('../../assets/Header-image.jpg')} alt='pizza mania Header' />
+                <div className='description'>
+                    <h2 className='text-center mt-1'>Delicious Pizza, Delievered To You</h2>
+                    <p>Choose your favorite pizza from our broad selection of avaliable pizza and enjoy a 
+                        delicious pizza at your place.</p>
+                    <p>All pizza are baked with high-quality ingredients,just-in-time and of course by experienced chef!</p>
+                </div>
+                    
+            </div>
 
         </>
     );

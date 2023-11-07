e467f15344f910b380f30618bf05796d36e6b298
jaiswalk008 <jaiskaran008@gmail.com>
2023-11-08 02:13:36 +0530

    made cart modal work and close button

--- src/Components/Cart/CartModalBackdrop.js
+++
@@ -1,9 +0,0 @@
-// Backdrop.js
-import React from 'react';
-import './Cart.css';    
-
-const CartModalBackdrop = ({ click }) => {
-  return <div className="backdrop" onClick={click}></div>;
-};
-
-export default CartModalBackdrop;
